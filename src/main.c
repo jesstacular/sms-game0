f4991a1dc9cc893c60ce3ac7c31dd2781afa29da
Jessica Cowie <63210422+jesstacular@users.noreply.github.com>
2023-01-08 11:59:08 +0000

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,35 @@
+#include "SMSlib.h"
+#ifdef USEPSGLIB                  // Set USEPSGLIB := true in Makefile to build with PSGlib for music and sounds
+#include "PSGlib.h"
+#endif
+#include "assets.generated.h"     // e.g. if using sverx's assets2banks utilitiy
+
+SMS_EMBED_SEGA_ROM_HEADER(0, 0);  //  includes the TMR SEGA header in the ROM image
+
+unsigned char paused = 0;
+
+void main(void)
+{
+  // game initialization here
+  
+  // main game loop
+  while (1)
+  {
+    SMS_waitForVBlank();
+    if (!paused)
+    {
+      // PSGFrame();                   // if using PSGlib you must call PSGFrame at *regular* intervals, e.g. here
+    }
+    SMS_copySpritestoSAT();
+    if (SMS_queryPauseRequested())
+    {
+      paused = !paused;
+      SMS_resetPauseRequest();
+    }
+
+    if (!paused)
+    {
+      // game logic here
+    }
+  }
+}
